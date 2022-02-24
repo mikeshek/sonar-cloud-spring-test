a797a32b1382a6b65842eaacd530dff90c141d0b
Michal Heczko <michal.heczko.ext@eng-sl.cz>
2022-02-24 15:39:27 +0100

    Spring boot init commit

---
+++ src/main/java/eu/mikesh/sonarcloudspringtest/SonarCloudSpringTestApplication.java
@@ -0,0 +1,13 @@
+package eu.mikesh.sonarcloudspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarCloudSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarCloudSpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/mikesh/sonarcloudspringtest/SonarCloudSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.mikesh.sonarcloudspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarCloudSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
