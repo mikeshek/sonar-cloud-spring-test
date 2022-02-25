3dd773ee2c8407782615fcdce142e578c9f31935
Michal Heczko <michal.heczko.ext@eng-sl.cz>
2022-02-25 11:43:17 +0100

    Main test updated to meet SonarQube rule

--- src/test/java/eu/mikesh/sonarcloudspringtest/SonarCloudSpringTestApplicationTests.java
+++ src/test/java/eu/mikesh/sonarcloudspringtest/SonarCloudSpringTestApplicationTests.java
@@ -1,13 +1,22 @@
 package eu.mikesh.sonarcloudspringtest;
 
+import eu.mikesh.sonarcloudspringtest.controller.TestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SonarCloudSpringTestApplicationTests {
 
+    @Autowired
+    private TestController testController;
+
+
     @Test
     void contextLoads() {
+        assertNotNull(testController);
     }
 
 }
